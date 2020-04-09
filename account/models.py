e33d45562405c684441751ba832eb26674f93c87
koieShoko <s19024@st.kic.ac>
2020-04-09 12:38:18 +0900

    after migrate

--- account/models.py
+++ account/models.py
@@ -78,7 +78,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['email']
 
 
     class Meta:
