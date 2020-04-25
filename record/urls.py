b8bc66fe724e30b31ed2e18e3188a7351dd1eb0a
koieShoko <s19024@st.kic.ac>
2020-04-25 22:37:23 +0900

    変換機能を実装したけど遅い

--- record/urls.py
+++ record/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('residents/',views.residents_list,name='residents_list'),
-    path('meal_record/read/',views.meal_record_read,name='meal_record_read')
-
+    path('meal_record/read/',views.meal_record_read,name='meal_record_read'),
+    path('meal_record/new/',views.meal_record_new,name='meal_record_new'),
+    path('meal_record/translate/<int:pk>/',views.meal_record_translate,name='meal_record_translate'),
 ]
--- record/views.py
+++ record/views.py
@@ -3,22 +3,38 @@ from .models import Meal_record
 from .forms import MealRecordForm
 from django.utils import timezone
 from record.models import Record
+from django.shortcuts import redirect
+from .translate import WordInfo, IikaeTango, Yomikomi, Translater, Kaigokiroku
+import csv
 
 def residents_list(request):
     a=1
     return render(request, 'record/residents_list.html',{'a':a})
 
-#def meal_record_new(request):
-    #if request.method =="POST":
- #   form=MealRecordForm()
-        #if form.is_valid():
-         #   meal_record=form.save(commit=False)#まだ保存しない
-          #  meal_record.residen
-           # meal_record.written_date=timezone.now()
-           # post.save()
-  #  return render(request, 'record/meal_record_edit.html', {'form':form})
+def meal_record_new(request):
+    if request.method =="POST":
+        form=MealRecordForm(request.POST)
+        if form.is_valid():
+            meal_record=form.save(commit=False)#まだ保存しない
+            meal_record.register()
+            return redirect('meal_record_translate',pk=meal_record.pk)
+    else:
+        form=MealRecordForm()
+    return render(request, 'record/meal_record_edit.html', {'form':form})
 
 
+def meal_record_translate(request,pk):
+#    string=Record.objects.get(pk=pk).notice
+#    string1=string.replace('開口','口の開き')
+#    translated_string=string1.replace('良好','よい')
+    string=Record.objects.get(pk=pk).notice
+    with open("/home/jinisuke55/record_of_care/record/string.csv","w") as f:
+        writer=csv.writer(f)
+        writer.writerow([string])
+    kiroku=Kaigokiroku()
+    translated_string=kiroku.translatedWords
+    #translated_string=string.replace("仰臥位","上を見て寝る姿勢")
+    return render(request, 'record/meal_record_translated.html', {'string':string,'translated_string':translated_string})
 
 
 
@@ -31,3 +47,4 @@ def meal_record_read(request):
            # meal_record.written_date=timezone.now()
            # post.save()
     return render(request, 'record/meal_record_read.html', {'string':string})
+
