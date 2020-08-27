44b0669e7b55e900d797ec760e453bd8a6938091
koieShoko <s19024@st.kic.ac>
2020-08-27 22:21:00 +0900

    改造前

--- record/models.py
+++ record/models.py
@@ -6,8 +6,10 @@ from account.models import User
 
 
 class Resident(models.Model):
-    full_name     = models.CharField(verbose_name="名前",max_length=128)
-    kana_full_name= models.CharField(verbose_name="ふりがな",max_length=128)
+    full_name          = models.CharField(
+        verbose_name   = "名前",
+        max_length     = 128)
+    kana_full_name     = models.CharField(verbose_name="ふりがな",max_length=128)
     DEPARTMENT_CHOICES = (#(保存する値,表示)
         ("1","1階"),
         ("2","2階"),
@@ -16,31 +18,31 @@ class Resident(models.Model):
         ("5","5階"),
         ("6","6階"),
     )
-    birthday        = models.DateField(
-        verbose_name="生年月日"
+    birthday         = models.DateField(
+        verbose_name ="生年月日"
     )
-    department      = models.CharField(
-        verbose_name="部署",
-        default="1",
-        max_length=128,
-        choices=DEPARTMENT_CHOICES,
+    department       = models.CharField(
+        verbose_name ="部署",
+        default      = "1",
+        max_length   = 128,
+        choices      = DEPARTMENT_CHOICES,
     )
-    UNIT_CHOICES    = (
+    UNIT_CHOICES     = (
         ('東','東'),
         ('西','西'),
     )
-    unit            = models.CharField(
-        verbose_name="ユニット",
-        default='東',
-        max_length=1,
-        choices=UNIT_CHOICES,
+    unit             = models.CharField(
+        verbose_name = "ユニット",
+        default      = '東',
+        max_length   = 1,
+        choices      = UNIT_CHOICES,
     )
-    room            = models.IntegerField(
-        verbose_name="部屋番号",
+    room             = models.IntegerField(
+        verbose_name = "部屋番号",
     )
-    is_leaving      = models.BooleanField(
-        verbose_name='退所済',
-        default=False,
+    is_leaving       = models.BooleanField(
+        verbose_name = '退所済',
+        default      = False,
     )
     def register(self):
         self.save()
@@ -50,8 +52,10 @@ class Resident(models.Model):
 
 
 class Staff(User):
-    full_name     = models.CharField(verbose_name="名前",max_length=128)
-    kana_full_name= models.CharField(verbose_name="ふりがな",max_length=128)
+    full_name          = models.CharField(
+        verbose_name   = "名前",
+        max_length     = 128)
+    kana_full_name     = models.CharField(verbose_name="ふりがな",max_length=128)
     DEPARTMENT_CHOICES = (#(保存する値,表示)
         ("1","1階"),
         ("2","2階"),
@@ -60,17 +64,17 @@ class Staff(User):
         ("5","5階"),
         ("6","6階"),
     )
-    department = models.CharField(
-        verbose_name="部署",
-        default="1",
-        max_length=128,
-        choices=DEPARTMENT_CHOICES,
+    department         = models.CharField(
+        verbose_name   = "部署",
+        default        = "1",
+        max_length     = 128,
+        choices        = DEPARTMENT_CHOICES,
     )
-    is_leaving      = models.BooleanField(
-        verbose_name='退所済かどうか',
-        default=False,
+    is_leaving         = models.BooleanField(
+        verbose_name   = '退所済かどうか',
+        default        = False,
     )
-    CHOICE_OCCUPATION=(
+    CHOICE_OCCUPATION  = (
         ("CS","介護職"),
         ("NS","看護師"),
         ("Dr","医師"),
@@ -78,114 +82,115 @@ class Staff(User):
         ("OT","作業療法士"),
         ("ST","言語聴覚士"),
     )
-    occupation = models.CharField(
-        verbose_name="職種",
-        choices=CHOICE_OCCUPATION,
-        max_length=128,
-        default="CS",
+    occupation         = models.CharField(
+        verbose_name   = "職種",
+        choices        = CHOICE_OCCUPATION,
+        max_length     = 128,
+        default        = "CS",
     )
-    reading_support=models.BooleanField(
-        verbose_name="日本語サポート",
-        default=False,
+    reading_support    = models.BooleanField(
+        verbose_name   = "日本語サポート",
+        default        = False,
     )
     def __str__(self):
         return self.full_name   
 
 class Record(models.Model):
-    resident        = models.ForeignKey(
+    resident           = models.ForeignKey(
         Resident,
-        verbose_name="利用者",
-        on_delete=models.CASCADE,
-        null=True,
+        verbose_name   = "利用者",
+        on_delete      = models.CASCADE,
+        null           = True,
     )
-    room            = models.IntegerField(
-        verbose_name="利用者の部屋番号",
-        default=0,
+    room               = models.IntegerField(
+        verbose_name   = "利用者の部屋番号",
+        default        = 0,
     )
-    staff           = models.ForeignKey(
+    staff              = models.ForeignKey(
         Staff,
-        verbose_name="職員",
-        on_delete=models.CASCADE,
-        blank=True, null=True
+        verbose_name   = "職員",
+        on_delete      = models.CASCADE,
+        blank          = True, 
+        null           = True
     )
-    date            = models.DateField(
-        verbose_name="日付",
-        default=timezone.now,
+    date               = models.DateField(
+        verbose_name   = "日付",
+        default        = timezone.now,
     )
-    time            = models.TimeField(
-        verbose_name="時刻",
-        default="07:00"
+    time               = models.TimeField(
+        verbose_name   = "時刻",
+        default        = "07:00"
     )
-    notice          = models.CharField(
-        verbose_name="特記事項",
-        max_length=8000,
-        default="",
-        blank=True
+    notice             = models.CharField(
+        verbose_name   = "特記事項",
+        max_length     = 8000,
+        default        = "",
+        blank          = True
     )
-    translated_notice= models.CharField(
-        verbose_name="変換結果",
-        max_length=8000,
-        default="",
-        blank=True,
+    translated_notice  = models.CharField(
+        verbose_name   = "変換結果",
+        max_length     = 8000,
+        default        = "",
+        blank          = True,
     )
     ruby_translated_notice = models.CharField(
-        verbose_name = "変換結果",
-        max_length   = 8000,
-        default = "",
-        blank   = True,
-        null    = True,
+        verbose_name   = "変換結果",
+        max_length     = 8000,
+        default        = "",
+        blank          = True,
+        null           = True,
     )
-    written_date    = models.DateTimeField(
-        blank=True, 
-        null=True
+    written_date       = models.DateTimeField(
+        blank          = True, 
+        null           = True
     )
-    department      = models.CharField(
-        verbose_name="部署",
-        max_length=128,
+    department         = models.CharField(
+        verbose_name   = "部署",
+        max_length     = 128,
     )
-    unit            = models.CharField(
-        verbose_name="ユニット",
-        max_length=128,
+    unit               = models.CharField(
+        verbose_name   = "ユニット",
+        max_length     = 128,
     )
-    isTranslated    = models.BooleanField(
-        verbose_name="変換済みかどうか",
-        default=False,
+    isTranslated       = models.BooleanField(
+        verbose_name   = "変換済みかどうか",
+        default        = False,
     )
     def register(self):
-        self.written_date=timezone.now()
-        self.department=self.resident.department
-        self.room=self.resident.room
-        self.unit=self.resident.unit
+        self.written_date  = timezone.now()
+        self.department    = self.resident.department
+        self.room          = self.resident.room
+        self.unit          = self.resident.unit
         self.save()
 
 
 
 class Meal_record(Record):
-    record_kind = "食事"
-    KIND_CHOICES   = (
+    record_kind      = "食事"
+    KIND_CHOICES     = (
         ('朝食','朝食'),
         ('昼食','昼食'),
         ('夕食','夕食'),
     )
-    form1           = models.CharField(
-        verbose_name="種類",
-        choices=KIND_CHOICES,
-        max_length=16,
-        default='朝食'
+    form1            = models.CharField(
+        verbose_name = "種類",
+        choices      = KIND_CHOICES,
+        max_length   = 16,
+        default      = '朝食'
     )
-    amounts=["{:>2}".format(str(x))+"/10" for x in range(0,11)]
-    FOOD_CHOICES=tuple([(x ,x ) for x in amounts])
-    form2           = models.CharField(
-        verbose_name="主食量",
-        default="10/10",
-        choices=FOOD_CHOICES,
-        max_length=16,
+    amounts          = ["{:>2}".format(str(x))+"/10" for x in range(0,11)]
+    FOOD_CHOICES     = tuple([(x ,x ) for x in amounts])
+    form2            = models.CharField(
+        verbose_name = "主食量",
+        default      = "10/10",
+        choices      = FOOD_CHOICES,
+        max_length   = 16,
     )
-    form3           = models.CharField(
-        verbose_name="副食量",
-        default="10/10",
-        choices=FOOD_CHOICES,
-        max_length=16,
+    form3            = models.CharField(
+        verbose_name = "副食量",
+        default      = "10/10",
+        choices      = FOOD_CHOICES,
+        max_length   = 16,
     )
     def __str__(self):
         return self.record_kind
--- record/static/js/javascript.js
+++ record/static/js/javascript.js
@@ -1,3 +1,34 @@
-function aaa(){
-    alert("aaaa");
-}
+function functionName()
+    {
+        var select1 = document.forms.formName.selectName1; //変数select1を宣言
+        var select2 = document.forms.formName.selectName2; //変数select2を宣言
+        var select3 = document.forms.formName.selectName3; //変数select3を宣言
+
+        
+        select2.options.length = 0; // 選択肢の数がそれぞれに異なる場合、これが重要
+        select3.options.length = 0; // 選択肢の数がそれぞれに異なる場合、これが重要
+           
+        var words = {果物:["りんご","みかん","オレンジ"], 野菜:["にんじん","キャベツ","きゅうり"]};
+        
+        if (select1.options[select1.selectedIndex].value == "果物")
+            {
+                select2.options[0] = new Option("りんご");
+                select2.options[1] = new Option("みかん");
+                select2.options[2] = new Option("オレンジ");
+            }
+         
+        else if (select1.options[select1.selectedIndex].value == "野菜")
+            {
+                select2.options[0] = new Option("キャベツ");
+                select2.options[1] = new Option("きゅうり");
+                select2.options[2] = new Option("にんんじん");
+                select2.options[3] = new Option("たまねぎ");
+            }
+         
+        else if (select1.options[select1.selectedIndex].value == "肉類")
+            {
+                select2.options[0] = new Option("豚肉");
+                select2.options[1] = new Option("牛肉");
+            }
+    }
+ 
